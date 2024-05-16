5ed2597737b2da4eac5e874c6850178146034592
John <50047655+jdunsmuir@users.noreply.github.com>
2024-05-16 17:56:37 +0100

    2024-05-16 17:56

--- ExecutionScript.js
+++ ExecutionScript.js
@@ -1,11 +1,8 @@
 let user = 'john';
-fetch('https://raw.githubusercontent.com/jdunsmuir/AutomatedSignup/main/ass.js')
+fetch('https://raw.githubusercontent.com/jdunsmuir/AutomatedSignup/main/ass.min.js')
   .then((response) => response.text())
   .then((code) => {
     let scriptElement = document.createElement('script');
-    scriptElement.textContent = code;
-    document.head.appendChild(scriptElement);
+    (scriptElement.textContent = code), document.head.appendChild(scriptElement);
   })
   .catch((e) => console.error(e));
-
-  fetch('https://raw.githubusercontent.com/jdunsmuir/AutomatedSignup/main/ass.min.js').then(r=>r.text()).then(code=>new Function(code)()).catch(console.error);
